39aeeee6b20833aacdf93d273e3a972bf8a7411c
Aymene Mansouri <aimenrma23@gmail.com>
2023-10-21 09:59:44 +0100

    added Projects folder

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -7,13 +7,27 @@ interface diskType {
   belong_to: number;
   name: string;
   type: string;
+  description: string;
+  link: string;
 }
 
 function trim(s: string) {
   return s.replaceAll(" ", "");
 }
+
+function getFile(disk: diskType[], name: string) {
+  for (let i = 0; i < disk.length; i++) {
+    if (disk[i].name == name) {
+      return disk[i];
+    }
+  }
+  return false;
+}
+
 export default function Terminal() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const cdDivRef = useRef<HTMLDivElement>();
+
   const [cdid, setCdid] = useState(0);
   const [path, setPath] = useState<string>("");
   const [cd, setCd] = useState<diskType>({
@@ -24,10 +38,11 @@ export default function Terminal() {
   });
 
   const [output, setOutput] = useState<React.ReactNode[]>([]);
-  const clearOutput = () => {
-    setOutput([]);
-  };
+  const [clear, setClear] = useState(false);
 
+  useEffect(() => {
+    setOutput([]);
+  }, [clear]);
   useEffect(() => {
     for (let i = 0; i < disk.length; i++) {
       if (disk[i].id == cdid) {
@@ -35,6 +50,15 @@ export default function Terminal() {
       }
     }
   }, [cdid]);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+  useEffect(() => {
+    cdDivRef.current?.scrollTo({
+      top: cdDivRef.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [output]);
 
   const proccessInput = (s: string) => {
     const command: string[] = s.split(" ");
@@ -51,11 +75,86 @@ export default function Terminal() {
 
     let content: diskType[];
 
-    switch (command[0]) {
+    switchLabel: switch (command[0]) {
+      case "cat":
+        if (command.length < 2) {
+          const err = (
+            <Error message={`Error : Missing file name`} path={path} />
+          );
+          toOut.push(err);
+          break;
+        } else if (command[1].trim() == "") {
+          const err = (
+            <Error message={`Error : Missing file name`} path={path} />
+          );
+          toOut.push(err);
+          break;
+        }
+        content = disk.filter((f) => f.belong_to == cdid);
+        const file = getFile(content, command[1]);
+        if (file) {
+          switch (file.type) {
+            case "project":
+              const fileOut: React.ReactNode = (
+                <div>
+                  <p className="ml-3 my-2 text-sky-400">
+                    <span className="font-semibold">Project description</span>{" "}
+                    {file.description}
+                    <br />
+                    <span className="font-semibold">
+                      Read full post at :{" "}
+                      <a
+                        className=" hover:underline"
+                        href={file.link}
+                        target="blank"
+                      >
+                        {file.name}
+                      </a>
+                    </span>{" "}
+                  </p>
+                </div>
+              );
+              toOut.push(fileOut);
+              break;
+            default:
+              const err = (
+                <Error message={`Error : cannot read folders`} path={path} />
+              );
+              toOut.push(err);
+              break;
+          }
+        } else {
+          const err = (
+            <Error
+              message={`Error : file '${command[1]}' not found`}
+              path={path}
+            />
+          );
+          toOut.push(err);
+        }
+        break;
       case "clear":
-        console.log(setOutput([]));
+        setClear(!clear);
         break;
       case "cd":
+        if (command.length < 2) {
+          const err = (
+            <Error message={`Error : Missing folder name`} path={path} />
+          );
+          toOut.push(err);
+          break;
+        } else if (command[1].trim() == "") {
+          const err = (
+            <Error message={`Error : Missing folder name`} path={path} />
+          );
+          toOut.push(err);
+          break;
+        }
+        if (command[1].trim() == "/") {
+          setPath("/");
+          setCdid(0);
+          break;
+        }
         if (command[1] == "..") {
           if (cdid == 0) {
             break;
@@ -67,29 +166,57 @@ export default function Terminal() {
         content = disk.filter((f) => f.belong_to == cdid);
         for (let i = 0; i < content.length; i++) {
           if (content[i].name == command[1]) {
+            if (content[i].type != "folder") {
+              const err = (
+                <Error
+                  message={`Error : ${command[1]} is not a folder`}
+                  path={path}
+                />
+              );
+              toOut.push(err);
+              break switchLabel;
+            }
             setPath(path + content[i].name + "/");
             setCdid(content[i].id);
-            break;
+            break switchLabel;
           }
-          const err = (
-            <Error message={`Error : Folder not found`} path={path} />
-          );
-          toOut.push(err);
-          console.log(toOut);
         }
+        const err2 = (
+          <Error
+            message={`Error : Folder ${command[1]} not found`}
+            path={path}
+          />
+        );
+        toOut.push(err2);
         break;
       case "ls":
+        const padding = 5;
         content = disk.filter((f) => f.belong_to == cdid);
         if (content.length == 0) {
           const err = <Error message={`Empty folder`} path={path} />;
           toOut.push(err);
           break;
         }
+        const longest = content.reduce((maxLength, item) => {
+          const nameLength = item.name.length;
+          return nameLength > maxLength ? nameLength : maxLength;
+        }, 0);
+        const longestType = content.reduce((maxLength, item) => {
+          const typeLength = item.type.length;
+          return typeLength > maxLength ? typeLength : maxLength;
+        }, 0);
+
         const filesList = (
-          <ul className=" list-disc">
+          <ul className=" whitespace-pre my-0">
             {" "}
+            <li>{"Name" + " ".repeat(longest + padding - 4) + "Type"}</li>
+            <li>{"-".repeat(longest + padding + longestType)}</li>
             {content.map((item) => (
-              <li key={item.id}>{item.name}</li>
+              <li key={item.id}>
+                {item.name}
+                {" ".repeat(longest + padding - item.name.length)}
+                {item.type}
+              </li>
             ))}{" "}
           </ul>
         );
@@ -97,7 +224,12 @@ export default function Terminal() {
         break;
 
       default:
-        const err = <Error message={`Error : Unknow command`} path={path} />;
+        const err = (
+          <Error
+            message={`Error : Unknow command '${command[0]}'`}
+            path={path}
+          />
+        );
         toOut.push(err);
     }
     setOutput([...output, ...toOut]);
@@ -115,9 +247,10 @@ export default function Terminal() {
   return (
     <div
       onClick={() => {
-        inputRef.current.focus();
+        inputRef.current?.focus();
       }}
-      className="h-full w-full px-3 py-1 text-sm hover:cursor-text backdrop-blur-[2px] bg-slate-900/75 rounded-b border border-t-0 border-slate-900 shadow-slate-900/20 shadow-md overflow-auto"
+      ref={cdDivRef}
+      className="h-[34rem]  w-full px-3 py-1 text-sm hover:cursor-text backdrop-blur-[2px] bg-slate-900/75 rounded-b border border-t-0 border-slate-900 shadow-slate-900/20 shadow-md overflow-y-scroll"
     >
       <div className="text-green-400">
         {output.map((dv) => (
